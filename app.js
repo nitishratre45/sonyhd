d9f88fa9dbce8e12dd4f97e3a4c1566b10dfedac
nitishratre45 <nitishratre45@gmail.com>
2026-09-20 00:24:19 +0530

    Update HLS stream URL in app.jssave

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // =========================================================
 
 const STREAM_URL =
-  "https://cloudplay-sonyliv.pages.dev/ten3hd.m3u8";
+  "https://cloudplay-sonyliv.pages.dev/ten3.m3u8";
 
 const video = document.getElementById("videoPlayer");
 const streamStatus = document.getElementById("streamStatus");
